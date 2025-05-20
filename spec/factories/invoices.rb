4ed9601a445891d94bccdef52769ad96f15d5ea4
Peter Bell <peter.bell5@ntlworld.com>
2025-05-20 22:38:43 +0100

    Adds invoice factory traits
    Adds a default date to the invoice factory for consistency.
    
    Also introduces a trait to associate an invoice with a client that has client sessions, streamlining testing scenarios that depend on this relationship.

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :invoice do
-    
+    date { Date.new(2024, 3, 16) }
+
+    trait :client_with_sessions do
+      client          { create :client, :with_client_sessions }
+      client_sessions { client.client_sessions }
+    end
   end
 end
