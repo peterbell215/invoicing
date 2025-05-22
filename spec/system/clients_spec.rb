9a92d29e1d10cd2bf7d36840d768d8ecc9f66d02
Peter Bell <peter.bell5@ntlworld.com>
2025-05-22 22:11:12 +0100

    Updates project dependencies and adds system test
    Updates project dependencies in the .iml file.
    
    Adds a system test for creating a new client, verifying
    successful creation and data integrity.  The test also
    includes a minor refactor for test data input.

--- spec/system/clients_spec.rb
+++ spec/system/clients_spec.rb
@@ -2,30 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "Clients", type: :system do
   describe "New client" do
+    subject(:new_client) { FactoryBot.build(:client) }
+
     it "allows adding a new client with valid information" do
       visit new_client_path
       
       # Fill in client information
-      fill_in "Name", with: "John Smith Consulting"
-      fill_in "Email", with: "john@smithconsulting.com"
-      fill_in "Address Line 1", with: "123 Business Lane"
-      fill_in "Address Line 2", with: "Suite 456"
-      fill_in "Town", with: "London"
-      fill_in "Postcode", with: "SW1A 1AA"
+      fill_in "Name", with: new_client.name
+      fill_in "Email", with: new_client.email
+      fill_in "Address Line 1", with: new_client.address1
+      fill_in "Town", with: new_client.town
+      fill_in "Postcode", with: new_client.postcode
       
       # Fill in billing information
       fill_in "Hourly Rate", with: "85.00"
       
       # Submit the form
       click_button "Create Client"
-      
-      # Expect to be redirected to the client's show page
+
+      # Read the created record and check that it matches what was entered.
       expect(page).to have_content("Client was successfully created")
-      expect(page).to have_content("John Smith Consulting")
-      expect(page).to have_content("john@smithconsulting.com")
-      expect(page).to have_content("123 Business Lane")
-      expect(page).to have_content("London")
-      expect(page).to have_content("SW1A 1AA")
+
+      created_client = Client.find_by(email: new_client.email)
+      expect(created_client).to have_attributes(name: new_client.name, email: new_client.email, address1: new_client.address1,
+        town: new_client.town, postcode: new_client.postcode)
+      expect(created_client.current_rate.to_s).to eq("85.00")
     end
 
     it "shows validation errors when submitting invalid information" do
