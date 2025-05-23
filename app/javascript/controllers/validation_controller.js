e4c18d8c4e22f592e126d69f9e3d8cab225853a8
Peter Bell <peter.bell5@ntlworld.com>
2025-05-23 21:48:03 +0100

    Adds client-side form validation
    Implements client-side validation for the client form using Stimulus.
    This provides immediate feedback to the user, improving the user experience and reducing unnecessary server requests.
    
    The validation controller handles the "invalid" event on input fields, adds a visual indication of the error, and displays an error message in the error explanation area.

---
+++ app/javascript/controllers/validation_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["errorExplanation"]
+
+  numberOfValidationErrors = 0;
+
+  connect() {
+
+  }
+
+  markFieldAsInvalid(event) {
+    event.preventDefault();
+
+    this.numberOfValidationErrors++;
+    event.target.classList.add("field-with-error");
+
+    this.addValidationErrorText(event.target);
+  }
+
+  addValidationErrorText(field) {
+    const errorContainer = this.errorExplanationTarget;
+
+    // Update error heading
+    var heading = errorContainer.querySelector("h2");
+
+    if (heading == null) {
+      errorContainer.innerHTML = `<h2></h2> <ul></ul>`
+      heading = errorContainer.querySelector("h2");
+    }
+
+    heading.innerText = `${this.numberOfValidationErrors} prohibited this client from being saved:`
+
+    // Clear existing error messages
+    const errorList = errorContainer.querySelector("ul");
+    const errorItem = document.createElement("li");
+    errorItem.textContent = `${field.labels[0].innerText} can't be blank`;
+    errorList.appendChild(errorItem);
+
+    errorContainer.hidden = false;
+  }
+}
