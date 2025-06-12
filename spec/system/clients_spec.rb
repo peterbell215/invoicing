98e85de5da31517c561ff5a1da8b19de295d5c33
Peter Bell <peter.bell5@ntlworld.com>
2025-06-12 21:15:58 +0100

    Refactors client form and validation tests
    Removes inline styles from the client session index view, promoting reusability and maintainability by leveraging existing CSS.
    
    Enhances client creation tests to ensure comprehensive validation, including postcode format, and improves clarity by removing redundant code.

--- spec/system/clients_spec.rb
+++ spec/system/clients_spec.rb
@@ -6,17 +6,17 @@ RSpec.describe "Clients", type: :system do
 
     it "allows adding a new client with valid information" do
       visit new_client_path
-      
+
       # Fill in client information
       fill_in "Name", with: new_client.name
       fill_in "Email", with: new_client.email
       fill_in "Address Line 1", with: new_client.address1
       fill_in "Town", with: new_client.town
       fill_in "Postcode", with: new_client.postcode
-      
+
       # Fill in billing information
       fill_in "Hourly Rate", with: "85.00"
-      
+
       # Submit the form
       click_button "Create Client"
 
@@ -31,30 +31,30 @@ RSpec.describe "Clients", type: :system do
 
     it "shows validation errors when submitting invalid information" do
       visit new_client_path
-      
+
       # Submit without filling any fields
       click_button "Create Client"
-      
+
       # Expect to see validation errors
       expect(page).to have_content("prohibited this client from being saved")
       expect(page).to have_content("Name can't be blank")
       expect(page).to have_content("Email can't be blank")
       expect(page).to have_content("Address Line 1 can't be blank")
       expect(page).to have_content("Town can't be blank")
-      
+
       # Fill in just one field and submit again
       fill_in "Name", with: "John Smith Consulting"
       click_button "Create Client"
-      
+
       # Should still show errors for other fields
       expect(page).to have_content("prohibited this client from being saved")
       expect(page).not_to have_content("Name can't be blank")
       expect(page).to have_content("Email can't be blank")
     end
-    
+
     it "validates postcode format" do
       visit new_client_path
-      
+
       # Fill in all required fields but with an invalid postcode
       fill_in "Name", with: "John Smith Consulting"
       fill_in "Email", with: "john@smithconsulting.com"
@@ -62,17 +62,17 @@ RSpec.describe "Clients", type: :system do
       fill_in "Town", with: "London"
       fill_in "Postcode", with: "invalid"
       fill_in "Hourly Rate", with: "85.00"
-      
+
       # Submit the form
       click_button "Create Client"
-      
+
       # Expect to see postcode validation error
       expect(page).to have_content("Postcode is badly formed postcode")
-      
+
       # Fix the postcode and try again
       fill_in "Postcode", with: "SW1A 1AA"
       click_button "Create Client"
-      
+
       # Should be successful now
       expect(page).to have_content("Client was successfully created")
     end
@@ -187,4 +187,3 @@ RSpec.describe "Clients", type: :system do
     end
   end
 end
-
