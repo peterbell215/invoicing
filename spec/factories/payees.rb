d8fc323ebe42dd0512993b6611b6d423680c7c0b
Peter Bell <peter.bell5@ntlworld.com>
2025-08-11 05:30:03 +0100

    testing: ensures Payee factory produces unique email addresses

--- spec/factories/payees.rb
+++ spec/factories/payees.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :payee do
-    name { "The Bursar" }
-    organisation { "The College" }
-    email { "bursar@college.edu" }
-    address1 { "Financial Office" }
-    address2 { "College Campus" }
-    town { "Cambridge" }
-    postcode { "CB2 1TN" }
-    phone { "01223 123456" }
-    active { true }
+    sequence(:name) { |n| "Testpayee #{n.to_words.capitalize }" }
+    organisation    { "The College" }
+    email           { "#{name&.parameterize(separator: ".") || 'test.client'}@example.com" }
+    address1        { "Financial Office" }
+    address2        { "College Campus" }
+    town            { "Cambridge" }
+    postcode        { "CB2 1TN" }
+    phone           { "01223 123456" }
+    active          { true }
   end
 end
 
