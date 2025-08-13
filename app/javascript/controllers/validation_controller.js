185a676a4a66f4e6efeed46fd4e051fe952c368a
Peter Bell <peter.bell5@ntlworld.com>
2025-08-13 20:33:58 +0100

    Improves form validation error messages
    Updates form validation to provide more specific error
    messages based on the type of validation failure.
    
    This provides better feedback to the user about why a
    field is invalid, improving the user experience.

--- app/javascript/controllers/validation_controller.js
+++ app/javascript/controllers/validation_controller.js
@@ -14,10 +14,30 @@ export default class extends Controller {
     this.numberOfValidationErrors++;
     event.target.classList.add("field-with-error");
 
-    this.addValidationErrorText(event.target);
+    if (event.currentTarget.validity.valueMissing) {
+        this.addValidationErrorText(event.currentTarget, "can't be blank");
+    } else if (event.currentTarget.validity.typeMismatch) {
+        this.addValidationErrorText(event.currentTarget, "is not a valid format");
+    } else if (event.currentTarget.validity.patternMismatch) {
+      this.addValidationErrorText(event.currentTarget, "format is invalid");
+    } else if (event.currentTarget.validity.tooLong) {
+      this.addValidationErrorText(event.currentTarget, "is too long");
+    } else if (event.currentTarget.validity.tooShort) {
+      this.addValidationErrorText(event.currentTarget, "is too short");
+    } else if (event.currentTarget.validity.rangeOverflow) {
+      this.addValidationErrorText(event.currentTarget, "is too high");
+    } else if (event.currentTarget.validity.rangeUnderflow) {
+      this.addValidationErrorText(event.currentTarget, "is too low");
+    } else if (event.currentTarget.validity.stepMismatch) {
+      this.addValidationErrorText(event.currentTarget, "is not a valid step");
+    } else if (event.currentTarget.validity.badInput) {
+      this.addValidationErrorText(event.currentTarget, "contains invalid characters");
+    } else if (event.currentTarget.validity.customError) {
+      this.addValidationErrorText(event.currentTarget, "is invalid");
+    }
   }
 
-  addValidationErrorText(field) {
+  addValidationErrorText(field, errorMessage) {
     // Update error heading
     let heading = this.errorExplanationTarget.querySelector("h2");
     heading.innerText = `${this.numberOfValidationErrors} prohibited this record from being saved:`
@@ -25,7 +45,7 @@ export default class extends Controller {
     // Clear existing error messages
     const errorList = this.errorExplanationTarget.querySelector("ul");
     const errorItem = document.createElement("li");
-    errorItem.textContent = `${field.labels[0].innerText} can't be blank`;
+    errorItem.textContent = `${field.labels[0].innerText} ${errorMessage}`;
     errorList.appendChild(errorItem);
 
     this.errorExplanationTarget.hidden = false;
